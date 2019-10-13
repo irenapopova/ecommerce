f2ce8201be44c9b4ed17eeb51ad17be8a75e3491
irenapopova <irenejpopova6@gmail.com>
2019-10-13 17:44:32 +0200

    fixes added

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,26 @@
 import Link from 'next/link';
+import NavStyle from `./styles/NavStyles`;
+
 
 const Nav = () => (
-    <div>
-         <Link href="/sell">
-            <a>Sell!</a>
+    <NavStyles>
+     <Link href="/items">
+         <a>Items</a>
          </Link>
-         <Link href="/">
-    <a>Home</a>
-     </Link>
-    </div>
+     <Link href="/sell">
+         </Link>
+      <Link href="/signup">
+         <a>Signup</a>
+         </Link>
+     <Link href="orders">
+         <a>Orders</a>
+         </Link>
+    <Link href="me">
+        <a>Account</a>
+      </Link>
+    
+   
+    </NavStyles>
 );
 
 export default Nav;
--- sick-fits/frontend/components/styles/NavStyles.js
+++ sick-fits/frontend/components/styles/NavStyles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+
+
 const NavStyles = styled.ul`
   margin: 0;
   padding: 0;
